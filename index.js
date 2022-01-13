a4c8a590e1b7e9d7b3632d97cde71dc221030432
Andrew <andrewsymes101@gmail.com>
2022-01-13 08:27:43 -0600

    add data persistance

--- index.js
+++ index.js
@@ -1,48 +1,76 @@
-'use strict';
+class CheckList extends React.Component {
 
-class ChecklistItem extends React.Component {
     constructor(props) {
         super(props)
+        var storedItems = JSON.parse(window.localStorage.getItem("items"))
         this.state = {
-            checked: false,
-            content: ""
+            list: storedItems ? storedItems : []
         }
     }
-    render() {
-        return React.createElement('div', { className: "check" },
-            React.createElement(
-                'button', {
-                    onClick: () => this.setState({ checked: !this.state.checked }),
-                    className: this.state.checked ? "checked" : "unchecked"
-                }),
-            React.createElement('input', { placeholder: "hi" })
-        )
-    }
-}
 
-class Checklist extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            items: [],
-            count: 0
-        }
+    saveItems = (obj) => {
+        window.localStorage.setItem("items", JSON.stringify(obj ? obj : this.state.list))
+    }
+
+    deleteItem = (id) => {
+        var copy = [...this.state.list]
+        copy.splice(id, 1)
+        this.setState({ list: copy })
+        this.saveItems(copy)
+    }
+
+    updateContent = (id) => {
+        var copy = [...this.state.list]
+        var content = document.querySelector("input[id='" + id + "']").value
+        copy[id].content = content
+        this.setState({ list: copy })
+        this.saveItems(copy)
+    }
+
+    toggleItem = (id) => {
+        var copy = [...this.state.list]
+        copy[id].checked = !copy[id].checked
+        this.setState({ list: copy })
+        this.saveItems(copy)
     }
 
     addItem = () => {
-        this.setState({
-            items: [...this.state.items, React.createElement(ChecklistItem, { key: this.state.count }, null)]
-        })
-        this.state.count += 1
+        var newList = [...this.state.list, { checked: false, content: "", key: uuidv4() }]
+        this.setState({ list: newList })
+        this.saveItems(newList)
     }
 
     render() {
-        return React.createElement('div', { className: "checklist" },
-            this.state.items,
-            React.createElement('button', { onClick: this.addItem }, 'New Item')
+        return (
+            React.createElement("div", {},
+                this.state.list.map((item, i) => {
+                    return React.createElement(ChecklistItem, { key: item.key, id: i, item: item, toggleItem: this.toggleItem, updateContent: this.updateContent, deleteItem: this.deleteItem })
+                }),
+                React.createElement("button", { className: "add-item", onClick: this.addItem }, "add")
+            )
         )
     }
 }
 
+function ChecklistItem({ item, id, toggleItem, updateContent, deleteItem }) {
+    function handleClick() {
+        toggleItem(id)
+    }
+
+    function handleTextInput() {
+        updateContent(id)
+    }
+
+    function deleteSelf() {
+        deleteItem(id)
+    }
+
+    return React.createElement("div", { className: "checklist-div" },
+        React.createElement("input", { type: "checkbox", className: "checkbox", defaultChecked: item.checked, onClick: () => { handleClick() } }),
+        React.createElement("input", { type: "text", className: "text-input", value: item.content, id: id, onChange: () => { handleTextInput() } }),
+        React.createElement("button", { className: "button-outline", onClick: () => { deleteSelf() } }, "remove")
+    )
+}
+
 const domContainer = document.querySelector('#container');
-ReactDOM.render(React.createElement(Checklist), domContainer);
+ReactDOM.render(React.createElement(CheckList), domContainer);
